65365d2ff12b574987703d16a3b885026c8fc49a
Vlad Lipen <nrj.504@gmail.com>
2013-07-02 22:58:31 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Poedem::Application.config.secret_key_base = 'ddf53505967c80f73dceeb20b9332d547ff9d3ebdce06e187803a26d2005bb3d9346daa5a014f12c6cf0e672cccfc4fa2269bb8f093394979838bfc13fb9c08a'
