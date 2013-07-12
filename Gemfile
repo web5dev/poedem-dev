6508576de5782e5e65c58df249ca4ebda1646c05
Vlad Lipen <nrj.504@gmail.com>
2013-07-12 22:52:48 +0300

    annotated User

--- Gemfile
+++ Gemfile
@@ -3,9 +3,15 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
-group :development do
+group :development, :test do
 	# Use sqlite3 as the database for Active Record
 	gem 'sqlite3'
+
+  gem 'rspec-rails'
+end
+
+group :development do
+  gem 'annotate'
 end
 
 group :production do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  login      :string(255)
+#  pass       :string(255)
+#  name       :string(255)
+#  email      :string(255)
+#  phone      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class User < ActiveRecord::Base
 end
