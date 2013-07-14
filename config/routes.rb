838dddd2d876efeccf59b659ccff3d173ca20a11
Vlad Lipen <nrj.504@gmail.com>
2013-07-14 19:13:35 +0300

    Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Poedem::Application.routes.draw do
+  get "users/new"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -13,7 +14,9 @@ Poedem::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :users
+
+  match '/signup',  to: 'users#new'
 
   # Example resource route with options:
   #   resources :products do
