4afe54dbf1987f1d0bdc779cdfb3ae3e71cae4d7
Vlad Lipen <nrj.504@gmail.com>
2013-07-15 22:06:18 +0300

    Sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Poedem::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   #get "welcome/index"
   get '/signup', to: 'users#new'
+  get '/signin',  to: 'sessions#new'
+  get '/signout', to: 'sessions#destroy', via: :delete
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
